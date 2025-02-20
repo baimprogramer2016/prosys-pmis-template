822864008bb2b6f754b9896e0c93f87b87b6b449
anhari <baimprogramer@gmail.com>
2025-02-20 16:18:38 +0700

    tes

--- resources/views/pages/mom/mom-pdf.blade.php
+++ resources/views/pages/mom/mom-pdf.blade.php
@@ -3,5 +3,5 @@
   <iframe id="pdfIframe" src="{{ asset('storage/' . $document->path)}}" style="width: 100%; height: 100vh; border: none;"></iframe>
 </div>
 <div class="modal-footer">
-  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-</div>
+  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+</div> 
