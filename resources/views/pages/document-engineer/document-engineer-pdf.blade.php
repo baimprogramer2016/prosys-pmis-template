375bd4e4960263f5140ddd601ec04c1fdbc354a6
anhari <baimprogramer@gmail.com>
2025-02-28 06:41:02 +0700

    word2

--- resources/views/pages/document-engineer/document-engineer-pdf.blade.php
+++ resources/views/pages/document-engineer/document-engineer-pdf.blade.php
@@ -1,16 +1,33 @@
-
 <div class="modal-body p-0">
+  <!-- Loading Indicator -->
+  <div id="loading" class="w-100 h-100 d-flex align-items-center justify-content-center">
+    <strong>Loading...</strong>
+  </div>
 
   @if(in_array($document->ext,['docx','doc']))
-  <iframe src="https://docs.google.com/gview?url={{ asset('storage/' . $document->path)}}&embedded=true" 
-        style="width:100%; height:600px;" frameborder="0">
-  </iframe>
+    <iframe id="docIframe" 
+            src="https://docs.google.com/gview?url={{ asset('storage/' . $document->path) }}&embedded=true" 
+            style="width:100%; height:600px; display: none;" 
+            frameborder="0"
+            onload="hideLoading()">
+    </iframe>
   @else
-    <iframe id="pdfIframe" src="{{ asset('storage/' . $document->path)}}" style="width: 100%; height: 100vh; border: none;"></iframe>
+    <iframe id="pdfIframe" 
+            src="{{ asset('storage/' . $document->path)}}" 
+            style="width: 100%; height: 100vh; border: none; display: none;" 
+            onload="hideLoading()">
+    </iframe>
   @endif
-  
-
 </div>
+
 <div class="modal-footer">
   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
 </div>
+
+<script>
+  function hideLoading() {
+    document.getElementById("loading").style.display = "none"; // Sembunyikan loading
+    document.getElementById("docIframe").style.display = "block"; // Tampilkan dokumen
+    document.getElementById("pdfIframe").style.display = "block"; // Tampilkan PDF
+  }
+</script>
