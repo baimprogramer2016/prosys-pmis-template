457b00a0cbfe5df9f806ad56cd150d2ed6ec513a
anhari <baimprogramer@gmail.com>
2025-02-28 06:13:06 +0700

    tes word preview2

--- resources/views/pages/document-engineer/document-engineer-pdf.blade.php
+++ resources/views/pages/document-engineer/document-engineer-pdf.blade.php
@@ -1,13 +1,13 @@
 
 <div class="modal-body p-0">
 
-  @if($document->ext === 'doxc'){
+  @if($document->ext === 'doxc')
     <iframe src="https://docs.google.com/gview?url={{ asset('storage/' . $document->path)}}&embedded=true" 
       style="width: 100%; height: 100vh; border: none;" frameborder="0">
     </iframe>
-  }else{
+  @else
     <iframe id="pdfIframe" src="{{ asset('storage/' . $document->path)}}" style="width: 100%; height: 100vh; border: none;"></iframe>
-  }
+  @endif
   
 
 </div>
