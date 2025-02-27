bdb2af73c1e89112fa290836a8819e85773e863b
anhari <baimprogramer@gmail.com>
2025-02-28 06:30:12 +0700

    tes Word

--- resources/views/pages/document-engineer/document-engineer-pdf.blade.php
+++ resources/views/pages/document-engineer/document-engineer-pdf.blade.php
@@ -2,7 +2,13 @@
 <div class="modal-body p-0">
 
   @if($document->ext === 'docx')
-  {!! $content !!}
+  
+  {{-- <div class="p-30 text-center">
+      {!! $content !!}
+  </div> --}}
+  <iframe src="https://docs.google.com/gview?url={{ asset('storage/' . $document->path)}}&embedded=true" 
+        style="width:100%; height:600px;" frameborder="0">
+  </iframe>
   @else
     <iframe id="pdfIframe" src="{{ asset('storage/' . $document->path)}}" style="width: 100%; height: 100vh; border: none;"></iframe>
   @endif
