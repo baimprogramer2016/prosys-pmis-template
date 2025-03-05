03f8938a279d6b0c16932a5d72f3b40a6e1d9019
anhari <baimprogramer@gmail.com>
2025-03-05 14:56:00 +0700

    Akses Invoice

--- resources/views/components/custom-invoice-record.blade.php
+++ resources/views/components/custom-invoice-record.blade.php
@@ -1,3 +1,4 @@
+@can('view_invoice_record')
 @foreach ($sidebar_invoice_record as $item_sidebar)
     <li class="nav-item">
         <a data-bs-toggle="collapse" href="#{{ $item_sidebar->id }}">
@@ -27,3 +28,4 @@
         </div>
     </li>
 @endforeach
+@endcan
